c49e4425e755cf357b87f17961fcb017f3f88daa
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-09 23:05:24 +0100

    initial build

---
+++ build-flexlab.ps1
@@ -0,0 +1,252 @@
+<#
+.Synopsis
+   labbuildr-flex allows you to create Virtual Machines with VMware Workstation from Predefined Scenarios.
+   Scenarios include Exchange 2013, SQL, Hyper-V, SCVMM .. .
+   labbuildr-flex runs on EMC VLAB Flex Environment
+.DESCRIPTION
+   labbuildr is a Self Installing Lab tool for Building VMware Virtual Machines on VMware Workstation
+      
+      Copyright 2015 Karsten Bott
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+.LINK
+   https://github.com/bottkars/labbuildr-flex
+#>
+[CmdletBinding(DefaultParametersetName = "version",
+    SupportsShouldProcess=$true,
+    ConfirmImpact="Medium")]
+	[OutputType([psobject])]
+param (
+    <#run build-lab update    #>
+	[Parameter(ParameterSetName = "update",Mandatory = $false, HelpMessage = "this will update labbuildr from latest git commit")][switch]$Update,
+    <#
+    run build-lab update    #>
+	[Parameter(ParameterSetName = "update",Mandatory = $false, HelpMessage = "select a branch to update from")][ValidateSet('master','testing','develop')]$branch  = "develop"
+)
+$Builddir = $PSScriptRoot
+
+try
+    {
+    [datetime]$Latest_labbuildr_flex_git = Get-Content  ($Builddir + "\labbuildr-flex-$branch.gitver") -ErrorAction Stop
+    }
+    catch
+    {
+    [datetime]$Latest_labbuildr_flex_git = "07/11/2015"
+    }
+try
+    {
+    [datetime]$Latest_labbuildr_scripts_git = Get-Content  ($Builddir + "\labbuildr-scripts-$branch.gitver") -ErrorAction Stop
+    }
+    catch
+    {
+    [datetime]$Latest_labbuildr_scripts_git = "07/11/2015"
+    }
+
+
+
+####
+function update-fromGit
+{
+
+
+	param (
+            [string]$Repo,
+            [string]$RepoLocation,
+            [string]$branch,
+            [string]$latest_local_Git,
+            [string]$Destination,
+            [switch]$delete
+            )
+
+
+        Write-Verbose "Using update-fromgit function for $repo"
+        $Uri = "https://api.github.com/repos/$RepoLocation/$repo/commits/$branch"
+        $Zip = ("https://github.com/$RepoLocation/$repo/archive/$branch.zip").ToLower()
+        $request = Invoke-WebRequest -UseBasicParsing -Uri $Uri -Method Head
+        [datetime]$latest_OnGit = $request.Headers.'Last-Modified'
+                Write-Verbose "We have $repo version $latest_local_Git, $latest_OnGit is online !"
+                if ($latest_local_Git -lt $latest_OnGit -or $force.IsPresent )
+                    {
+                    $Updatepath = "$Builddir\Update"
+					if (!(Get-Item -Path $Updatepath -ErrorAction SilentlyContinue))
+					        {
+						    $newDir = New-Item -ItemType Directory -Path "$Updatepath"
+                            }
+                    Write-Output "We found a newer Version for $repo on Git Dated $($request.Headers.'Last-Modified')"
+                    if ($delete.IsPresent)
+                        {
+                        Write-Verbose "Cleaning $Destination"
+                        Remove-Item -Path $Destination -Recurse -ErrorAction SilentlyContinue
+                        }
+                    Get-LABHttpFile -SourceURL $Zip -TarGetFile "$Builddir\update\$repo-$branch.zip" -ignoresize
+                    Expand-LABZip -zipfilename "$Builddir\update\$repo-$branch.zip" -destination $Destination -Folder $repo-$branch
+                    $Isnew = $true
+                    $request.Headers.'Last-Modified' | Set-Content ($Builddir+"\$repo-$branch.gitver") 
+                    }
+                else 
+                    {
+                    Write-Warning "No update required for labbuildr-flex, already newest version "
+                    }
+
+}
+#####
+function Get-LABHttpFile
+ {
+    [CmdletBinding(DefaultParametersetName = "1",
+    HelpUri = "https://github.com/bottkars/LABbuildr/wiki/LABtools#GET-LABHttpFile")]
+	param (
+	[Parameter(ParameterSetName = "1", Mandatory = $true,Position = 0)]$SourceURL,
+    [Parameter(ParameterSetName = "1", Mandatory = $false)]$TarGetFile,
+    [Parameter(ParameterSetName = "1", Mandatory = $false)][switch]$ignoresize
+    )
+
+
+begin
+{}
+process
+{
+if (!$TarGetFile)
+    {
+    $TarGetFile = Split-Path -Leaf $SourceURL
+    }
+try
+                    {
+                    $Request = Invoke-WebRequest $SourceURL -UseBasicParsing -Method Head
+                    }
+                catch [Exception] 
+                    {
+                    Write-Warning "Could not downlod $SourceURL"
+                    Write-Warning $_.Exception
+                    break
+                    }
+                
+                $Length = $request.Headers.'content-length'
+                try
+                    {
+                    # $Size = "{0:N2}" -f ($Length/1GB)
+                    # Write-Warning "
+                    # Trying to download $SourceURL 
+                    # The File size is $($size)GB, this might take a while....
+                    # Please do not interrupt the download"
+                    Invoke-WebRequest $SourceURL -OutFile $TarGetFile
+                    }
+                catch [Exception] 
+                    {
+                    Write-Warning "Could not downlod $SourceURL. please download manually"
+                    Write-Warning $_.Exception
+                    break
+                    }
+                if ( (Get-ChildItem  $TarGetFile).length -ne $Length -and !$ignoresize)
+                    {
+                    Write-Warning "File size does not match"
+                    Remove-Item $TarGetFile -Force
+                    break
+                    }                       
+
+
+}
+end
+{}
+}                 
+###
+function Expand-LABZip
+{
+ [CmdletBinding(DefaultParameterSetName='Parameter Set 1',
+    HelpUri = "https://github.com/bottkars/LABbuildr/wiki/LABtools#Expand-LABZip")]
+	param (
+        [string]$zipfilename,
+        [string] $destination,
+        [String]$Folder)
+	$copyFlag = 16 # overwrite = yes
+	$Origin = $MyInvocation.MyCommand
+	if (test-path($zipfilename))
+	{
+    If ($Folder)
+        {
+        $zipfilename = Join-Path $zipfilename $Folder
+        }
+    		
+        Write-Verbose "extracting $zipfilename to $destination"
+        if (!(test-path  $destination))
+            {
+            New-Item -ItemType Directory -Force -Path $destination | Out-Null
+            }
+        $shellApplication = New-object -com shell.application
+		$zipPackage = $shellApplication.NameSpace($zipfilename)
+		$destinationFolder = $shellApplication.NameSpace("$destination")
+		$destinationFolder.CopyHere($zipPackage.Items(), $copyFlag)
+	}
+}
+
+##
+function Extract-Zip
+{
+	param ([string]$zipfilename, [string] $destination)
+	$copyFlag = 16 # overwrite = yes
+	$Origin = $MyInvocation.MyCommand
+	if (test-path($zipfilename))
+	{		
+        if (!(Test-Path $destination))
+            {New-Item -ItemType Directory -Path $destination -Force | Out-Null }
+        Write-Verbose "extracting $zipfilename"
+        $shellApplication = new-object -com shell.application
+		$zipPackage = $shellApplication.NameSpace($zipfilename)
+		$destinationFolder = $shellApplication.NameSpace($destination)
+		$destinationFolder.CopyHere($zipPackage.Items(), $copyFlag)
+	}
+}
+####
+
+switch ($PsCmdlet.ParameterSetName)
+{
+    "update" 
+        {
+
+
+        $Repo = "labbuildr-flex"
+        $RepoLocation = "bottkars"
+        $Latest_local_git = $Latest_labbuildr_flex_git
+        $Destination = "$Builddir"
+        update-fromGit -Repo $Repo -RepoLocation $RepoLocation -branch $branch -latest_local_Git $Latest_local_git -Destination $Destination
+        if (Test-Path "$Builddir\deletefiles.txt")
+		    {
+			$deletefiles = get-content "$Builddir\deletefiles.txt"
+			foreach ($deletefile in $deletefiles)
+			    {
+				if (Get-Item $Builddir\$deletefile -ErrorAction SilentlyContinue)
+				    {
+					Remove-Item -Path $Builddir\$deletefile -Recurse -ErrorAction SilentlyContinue
+					status "deleted $deletefile"
+					write-log "deleted $deletefile"
+					}
+			    }
+            }
+        else 
+            {
+            Write-Host "No Deletions required"
+            }
+        $Repo = "labbuildr-scripts"
+        $RepoLocation = "bottkars"
+        $Latest_local_git = $Latest_labbuildr_scripts_git
+        $Destination = "$Builddir\Scripts"
+        update-fromGit -Repo $Repo -RepoLocation $RepoLocation -branch $branch -latest_local_Git $Latest_local_git -Destination $Destination -delete
+
+            return
+    }# end Updatefromgit
+
+}
+
+
+
+
+
